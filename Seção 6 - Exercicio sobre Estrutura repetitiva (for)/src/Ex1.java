f26dfc29f3e23b1a48a29dc3a372dc66262e41d9
Bettim <Vitor.bettim123@gmail.com>
2026-01-24 16:32:39 -0300

    Add files via upload

---
+++ Seção 6 - Exercicio sobre Estrutura repetitiva (for)/src/Ex1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ex1 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        for(int i=1; i<=x; i++){
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
